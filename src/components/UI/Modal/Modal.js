beb8eafed4a5da63d666cd14e46043af8a698f87
Fabio Mezzina <fabio.mezzina.09@gmail.com>
2021-04-21 23:19:43 +0200

    shouldComponentUpdate -> Modal - preventing re-rendering

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Aux>
-    <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div className={classes.Modal} style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: props.show ? '1' : '0'}}>
-      {props.children}
-  </div>
-  </Aux>
-);
+class Modal extends Component {
+  // se ho ho una modifica nella props show (quella che mostra modale e backdrop) allora vado avanti con il render
+  // altrimenti non verrà fatto il re-render della modale, nè quindi del backdrop e nemmeno di OrderSummary (presente nella Modal in BurgerBuilder)
+  shouldComponentUpdate(nextProps, nextState) {
+    if(nextProps.show !== this.props.show) {
+      return true;
+    }
+  }
+  
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+        <div className={classes.Modal} style={{transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)', opacity: this.props.show ? '1' : '0'}}>
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
